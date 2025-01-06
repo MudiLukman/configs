5e05883b48e6f136b08f8e140d10f5e3457be739
Mudi Lukman <mudi.lukman.developer@gmail.com>
2025-01-06 15:22:11 +0100

    Update emacs config: init.el with java development configurations

--- emacs/init.el
+++ emacs/init.el
@@ -2,7 +2,7 @@
 (setq inhibit-startup-message t)
 
 (global-display-line-numbers-mode t) ; Display line numbers
-;;Disbale line numbers for some modes i.e shell mode
+;; Disbale line numbers for some modes i.e shell mode
 (dolist (mode '(org-mode-hook
 		term-mode-hook
 		shell-mode-hook
@@ -10,6 +10,11 @@
 		pdf-view-mode-hook
 		eshell-mode-hook))
  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; Disable word wrap
+(add-hook 'prog-mode-hook (lambda () (setq truncate-lines t)))
+(add-hook 'text-mode-hook (lambda () (setq truncate-lines t)))
+(add-hook 'lsp-mode-hook (lambda () (setq truncate-lines t)))
 
 ;; All Files
 (save-place-mode 1) ;; Remember to continue from previously open page
@@ -28,6 +33,13 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(use-package eterm-256color
+  :hook (term-mode . eterm-256color-mode))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+
 ;;PDF Config
 (use-package pdf-tools
   :ensure t
@@ -43,94 +55,40 @@
 
 ;;TODO: Disable word wrap for prog-mode i.e. source codes & text mode
 
-
-(use-package eterm-256color
-  :hook (term-mode . eterm-256color-mode))
-
-;;install and configure lsp-mode (language server protocol) for Java
-;; This wasn't straigth forward at all but after I cd into ~/.emacs.d/.cache/lsp/jdtls
-;; Then I wget http://download.eclipse.org/jdtls/snapshots/jdt-language-server-1.44.0-202412192041.tar.gz
-;; Then tar -xzf jdt-language-server-1.44.0-202412192041.tar.gz
-;; Then set (setq lsp-java-server-install-dir "~/.emacs.d/.cache/lsp/jdtls")
-(use-package lsp-mode
-  :hook ((java-mode . lsp))
-  :commands (lsp lsp-deferred)
-  :config
-  (setq lsp-keymap-prefix "C-c l"
-        lsp-enable-snippet nil
-        lsp-java-import-order '["java" "javax" "com" "org"]
-        lsp-java-save-actions-organize-imports t
-        lsp-java-completion-enabled t
-        lsp-java-autobuild-enabled t
-        lsp-java-format-enabled t))
-
-;; Autocomplete with Company Mode
-(use-package company
-  :hook (lsp-mode . company-mode)
-  :config
-  (setq company-idle-delay 0.1
-        company-minimum-prefix-length 1
-        company-tooltip-align-annotations t
-        company-backends '(company-capf)))
-
-;; Optional: UI Enhancements with LSP-UI
-(use-package lsp-ui
-  :commands lsp-ui-mode
-  :hook (lsp-mode . lsp-ui-mode)
-  :config
-  (setq lsp-ui-doc-enable t
-        lsp-ui-doc-show-with-cursor t
-        lsp-ui-sideline-enable t
-        lsp-ui-peek-enable t))
-
-;; Flycheck for Real-Time Syntax Checking
-(use-package flycheck
-  :hook (lsp-mode . flycheck-mode))
-
-;; Debugging with DAP Mode
-(use-package dap-mode
-  :after lsp-mode
-  :config
-  (dap-auto-configure-mode)
-  (require 'dap-java)
-  (setq dap-java-test-runner "junit")) 
-
-;; Project Management with Treemacs
-(use-package treemacs
-  :commands treemacs
-  :config
-  (setq treemacs-is-never-other-window t))
-
-(use-package lsp-treemacs
-  :after (lsp-mode treemacs)
-  :config
-  (lsp-treemacs-sync-mode 1))
-
-;; Keybinding Hints with Which-Key
 (use-package which-key
-  :config
-  (which-key-mode))
+   :config
+   (which-key-mode))
 
 ;; Quick Commenting with Evil-Nerd-Commenter
 (use-package evil-nerd-commenter
-  :bind ("M-/" . evilnc-comment-or-uncomment-lines))
+   :bind ("M-/" . evilnc-comment-or-uncomment-lines))
+
+(use-package projectile)
+(use-package flycheck)
+(use-package yasnippet :config (yas-global-mode))
+(use-package lsp-mode :hook ((lsp-mode . lsp-enable-which-key-integration)))
+(use-package hydra)
+(use-package company)
+(use-package lsp-ui)
+(use-package which-key :config (which-key-mode))
+(use-package lsp-java :config (add-hook 'java-mode-hook 'lsp))
+(use-package dap-mode :after lsp-mode :config (dap-auto-configure-mode))
+(use-package dap-java :ensure nil)
+(use-package helm-lsp)
+(use-package helm
+  :config (helm-mode))
+(use-package lsp-treemacs)
+
+(use-package magit
+  :custom
+  (magit-display-buffer-fuction #'magit-display-buffer-same-window-except-diff-v1))
+
+(use-package evil-magit
+  :after magit)
 
 (setq lsp-java-java-path "/usr/lib/jvm/java-21-openjdk/bin/java")
 (setq lsp-java-server-install-dir "~/.emacs.d/.cache/lsp/jdtls")
-
-;; Refactoring Support
-(use-package lsp-java
-  :after lsp-mode
-  :config
-  (setq lsp-java-format-settings-url "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml"
-        lsp-java-format-settings-profile "GoogleStyle"))
-
-;; Snippets with YASnippet (Optional)
-(use-package yasnippet
-  :hook (java-mode . yas-minor-mode))
-
-;; Additional Settings
-(setq lsp-java-vmargs '("-Xmx2G" "-XX:+UseG1GC" "-XX:+UseStringDeduplication")) 
+(setq lsp-java-vmargs (list "-javaagent:/home/kontrol/Projects/libs/lombok.jar"))
 
 (scroll-bar-mode -1) ; Disable Scroll bar
 (tool-bar-mode -1) ; Disable toobar
@@ -147,7 +105,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(lsp-docker lsp-javacomp lsp-java dap-mode eterm-256color pdf-tools evil-nerd-commenter lsp-treemacs treemacs which-key flycheck lsp-ui company lsp-mode))
+   '(evil-magit magit nerd-fonts all-the-icons-nerd-fonts doom-modeline helm helm-lsp lsp-docker lsp-javacomp lsp-java dap-mode eterm-256color pdf-tools evil-nerd-commenter lsp-treemacs treemacs which-key flycheck lsp-ui company lsp-mode))
  '(warning-suppress-log-types '((use-package)))
  '(warning-suppress-types '((use-package))))
 (custom-set-faces
